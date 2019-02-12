25422b8177598b24b693f8a949ee2921888eac04
Cstzhangzb <devzhangzb@163.com>
2019-02-12 15:13:06 +0800

    c practice
    c practice

---
+++ 01_C/cmake-build-debug/CMakeFiles/01_C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/01_C.dir/main.c.o"
+  "01_C.pdb"
+  "01_C"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/01_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
