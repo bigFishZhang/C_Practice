eeb1fee10e76ea6272f63bc2d15c45b44c048340
Cstzhangzb <devzhangzb@163.com>
2019-02-26 16:30:52 +0800

    printf

--- untitled/main.c
+++ untitled/main.c
@@ -5,11 +5,26 @@ int main() {
 
 //    float  a = 100.9f;
 //    printf("a = %f\n",a);
-    printf("请输入：");
-    float  a;
+//    printf("请输入：");
+//    float  a;
+//
+//    scanf("%f",&a);
+//
+//    printf("a = %f\n",a);
 
-    scanf("%f",&a);
-    printf("a = %f\n",a);
+
+    short  int b; //2个字节
+    b = 2;
+    printf("%hd  \n",b);
+
+    long c ;//4字节
+    c = 1;
+    printf("%ld  \n",c);
+
+    long long d; //8个字节
+    d = 1000;
+    printf("%lld \n",d);
+
 
 
 
