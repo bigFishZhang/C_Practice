6e1288f658f3ebc5edc6f65e92cb6d9951bd6a04
zzb <devzhangzb@163.com>
2019-04-21 12:22:57 +0800

    var1

--- c0421/hello.c
+++ c0421/hello.c
@@ -5,8 +5,10 @@ int main(int argc,char* argv[])
 	int a = 100;
 	float b=5.89;
 	char c = 'Y';
+	const int xx = 200;
 	printf("hello c \n");
 	printf("a=%d\n",a);
+	printf("a=%d\n",xx);
 	printf("b=%f\n",b);
 	printf("c=%c\n",c);
 	return 0;
---
+++ c0421/var1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc,char* argv[])
+{
+	int a = 0;
+	printf("a=%d\n",a);
+	a = 10;
+	printf("a=%d\n",a);
+	const int x = 100;
+	printf("x=%d\n",x);
+	return 0;
+
+}
+
+
